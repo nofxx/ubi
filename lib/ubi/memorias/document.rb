84fbe68c037a4d6266694b331c3b2280ac2c4818
Marcos Piccinini <x@nofxx.com>
2014-12-25 08:35:24 -0200

    document

--- lib/ubi/memorias/document.rb
+++ lib/ubi/memorias/document.rb
@@ -1,13 +1,48 @@
 module Ubi
   module Memoria
     # Show me your papers!
+    #
+    # https://en.wikipedia.org/wiki/National_identification_number
+    #
+    # br: CNPJ
+    # us: SSN
+    # cl: RUN/RUT
+    #
     class Document < Base
+      #
+      #
+      # Init, remove non word chars
+      #
+      def initialize(value)
+        @value = value.gsub(/\W/, '')
+      end
+
+      def format(location = :br)
+        value.sub(*self.class.formats[location])
+      end
+
+      #
+      # Class methods
+      #
       class << self
         def regexes
           {
-            br: /^\d{14}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/,
-            us: /(\d{3}[-]\d{2}[-]\d{4})/
+            br: /(?:\d{14}|\d{2}\.?\d{3}\.?\d{3}\/?\d{4}[-]?\d{2})/,
+            cl: /\d{2}\.\d{3}\.\d{3}[-][0-9kK]/,
+            us: /\d{3}[-]\d{2}[-]\d{4}/
           }
+        end
+
+        def formats
+          {
+            br: [/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '\1.\2.\3/\4-\5'],
+            cl: [/(\d{2})(\d{3})(\d{3})(\d{1})/, '\1.\2.\3-\4'],
+            us: [/(\d{3})(\d{2})(\d{4})/, '\1-\2-\3']
+          }
+        end
+
+        def regex(location = :br)
+          regexes[location]
         end
 
         def key
--- spec/ubi/memorias/document_spec.rb
+++ spec/ubi/memorias/document_spec.rb
@@ -1,4 +1,48 @@
 require 'spec_helper'
 
 describe Memoria::Document do
+  describe 'simple test' do
+    subject { Memoria::Document.parse('bla bla 5 12.345.678/0001-90 79 so') }
+
+    it { is_expected.to include(Memoria::Document) }
+    it { is_expected.to be_an Array }
+  end
+
+  describe 'brazilian CNPJ' do
+    def parse(document)
+      Memoria::Document.parse(document).first.to_s
+    end
+
+    let(:parsed) { '12.345.678/0001-90' }
+
+    it { expect(parse('12345678000190')).to eq(parsed) }
+    it { expect(parse('123456780001-90')).to eq(parsed) }
+    it { expect(parse('12345678/0001-90')).to eq(parsed) }
+    it { expect(parse('12345.678/0001-90')).to eq(parsed) }
+    it { expect(parse('12.345.678/0001-90')).to eq(parsed) }
+  end
+
+  describe 'not documents' do
+    %w(
+      1234
+      12345
+      123456
+      1234567
+      123@email.com
+      11982345678
+      11 982345678
+      11-982345678
+      11.982345678
+      11-98234-5678
+      11  982345678
+      (11)982345678
+      (11)98234-5678
+      (11) 98234-5678
+      (11) 98234.5678
+    ).each do |bad_document|
+      it "should not parse '#{bad_document}'" do
+        expect(Memoria::Document.parse(bad_document).first).to be_nil
+      end
+    end
+  end
 end
