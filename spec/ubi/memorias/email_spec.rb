eba56f573d5697ee60ff144846bd1e567559b9a5
Marcos Piccinini <x@nofxx.com>
2014-12-25 05:10:46 -0200

    email

--- spec/ubi/memorias/email_spec.rb
+++ spec/ubi/memorias/email_spec.rb
@@ -4,6 +4,48 @@ describe Memoria::Email do
   describe 'simple test' do
     subject { Memoria::Email.parse('bla bla me@somesite.com') }
 
+    it { is_expected.to include(Memoria::Email) }
     it { is_expected.to be_an Array }
+  end
+
+  describe 'valid' do
+    %w(
+      foo@foo.com
+      foo@foo.org
+      foo@foo.net
+      foo_bar@foo.net
+      foo-bar@foo.net
+      foo.bar@foo.net
+      foo.bar9@foo.net
+      foo.bar@99foo.net
+      zumbi@land.com.br
+      zumbi@land.org.br
+      zumbi@land.net.br
+      zum@dom.land.co.uk
+      zum@ad.dom.land.co.tk
+    ).each do |good_mail|
+      it "should correctly parse '#{good_mail}'" do
+        res = Memoria::Email.parse(good_mail)
+        expect(res.size).to eq(1)
+        expect(res.first.to_s).to eq(good_mail)
+      end
+    end
+  end
+
+  describe 'invalid' do
+    %w(
+      @foo
+      foo@foo
+      @foo.org
+      zumbi@.com
+      foo@@foo.com
+      zum.@good.com
+      @11 53 2355
+      @11532355
+    ).each do |bad_mail|
+      it "should not parse '#{bad_mail}'" do
+        expect(Memoria::Email.parse(bad_mail).first).to be_nil
+      end
+    end
   end
 end
